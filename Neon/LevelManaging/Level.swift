1755d79b234295b224a658ae945ede3cc87b5e79
Penjat <theSpencerSymington@Gmail.com>
2019-02-14 22:39:19 -0800

    added core data and level structures

--- Neon/LevelManaging/Level.swift
+++ Neon/LevelManaging/Level.swift
@@ -2,12 +2,12 @@
 
 import Foundation
 
-struct Level{
-  let segments :[Segment]
-  
-  func getNextSegment() -> Segment{
-    //get a random segment
-    let randomIndex = Int.random(in: 0..<segments.count)
-    return segments[randomIndex]
-  }
-}
+//struct Level{
+//  let segments :[Segment]
+//  
+//  func getNextSegment() -> Segment{
+//    //get a random segment
+//    let randomIndex = Int.random(in: 0..<segments.count)
+//    return segments[randomIndex]
+//  }
+//}
--- Neon/LevelManaging/PieceFactory.swift
+++ Neon/LevelManaging/PieceFactory.swift
@@ -12,14 +12,14 @@ class PieceFactory{
   init(gameScene: GameScene) {
     self.gameScene = gameScene
     
-    let piece = Piece(position:CGPoint(x: 0,y: 0 ) , size: CGSize(width: 2, height: 1), collectType: CollectType.DamagePlayer)
-    let segment = Segment(pieces: [piece] )
-    
-    let piece1 = Piece(position:CGPoint(x: 6,y: 0 ) , size: CGSize(width: 2, height: 1), collectType: CollectType.Points)
-    let piece2 = Piece(position:CGPoint(x: -6,y: 0 ) , size: CGSize(width: 2, height: 1), collectType: CollectType.Points)
-    
-    let segment1 = Segment(pieces: [piece1,piece2] )
-    curLevel = Level(segments: [segment,segment1])
+//    let piece = Piece(position:CGPoint(x: 0,y: 0 ) , size: CGSize(width: 2, height: 1), collectType: CollectType.DamagePlayer)
+//    let segment = Segment(pieces: [piece] )
+//    
+//    let piece1 = Piece(position:CGPoint(x: 6,y: 0 ) , size: CGSize(width: 2, height: 1), collectType: CollectType.Points)
+//    let piece2 = Piece(position:CGPoint(x: -6,y: 0 ) , size: CGSize(width: 2, height: 1), collectType: CollectType.Points)
+//    
+//    let segment1 = Segment(pieces: [piece1,piece2] )
+//    curLevel = Level(segments: [segment,segment1])
     
   }
   
@@ -40,10 +40,10 @@ class PieceFactory{
 //    let point = CGPoint(x:randX , y: gameScene.frame.height/2)
     
     
-    let segment = curLevel.getNextSegment()
-    for piece in segment.pieces{
-      gameScene.createPieceNode(piece:piece)
-    }
+//    let segment = curLevel.getNextSegment()
+//    for piece in segment.pieces{
+//      gameScene.createPieceNode(piece:piece)
+//    }
     
     
   }
